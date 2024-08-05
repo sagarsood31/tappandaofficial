a40da3e1aaed9b288ef81632eef5b83b42f4cb04
Sagar Sood <moneyomatics@gmail.com>
2024-08-06 00:33:28 +0530

    sdfs

--- routes/gameState.js
+++ routes/gameState.js
@@ -1,12 +1,13 @@
 // routes/gameState.js
 
 import express from 'express';
-import { getGameState, updateGameState, resetGameState } from '../controllers/gameStateController.js';
+import { getGameState, updateGameState, resetGameState, verifyInvite } from '../controller/gameStateController.js';
 
 const router = express.Router();
 
 router.get('/:userId', getGameState);
 router.post('/update/:userId', updateGameState);
 router.post('/reset/:userId', resetGameState);
+router.post('/verify-invite', verifyInvite);
 
 export default router;
